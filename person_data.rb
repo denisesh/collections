0714d3c283617afd6e3761ed8b06fb66eb48e508
denisesh <denisesh@Denises-MacBook-Air.local>
2014-10-31 14:08:05 -0700

    initial commit

---
+++ person_data.rb
@@ -0,0 +1,67 @@
+
+ary=[
+{first_name: "John",
+last_name: "Foley",
+email: "john@gschool.it",
+class: "Beginning snark"},
+
+{first_name:"Sylvester",
+last_name: "Kelsey",
+email: "sellie@gmail.com",
+class: "Ruby Immersive"},
+
+{first_name:"Timothy",
+last_name: "Rama",
+email: "tim.rama@gmail.com",
+class:"Ruby Immersive"},
+
+{first_name: "Kane",
+last_name: "Baccigalupi",
+email: "kane@gschool.it",
+class: "C for dummies"},
+
+{first_name: "Nikita",
+last_name: "Theodosius",
+email: "nikita.theo@gmail.com",
+class:"Ruby Immersive"},
+
+{first_name: "Roddy",
+last_name: "Eldred",
+email: "roddy.el@gmail.com",
+class: "Ruby Immersive"},
+
+{first_name: "Martha",
+last_name: "Berner",
+email: "martha@gschool.it",
+class: "Time travel for beginners"},
+
+{first_name: "Kofi",
+last_name: "Thomas",
+email: "k.thomas@hotmail.com",
+class: "Ruby Immersive"}
+]
+puts "--------------ALL-----------------------"
+ary.each do |hash|
+  puts "#{hash[:last_name]} #{hash[:first_name]} #{hash[:email]}"
+end
+puts "-------------------------------SOME-----------------"
+
+ary.each do |hash|
+  if hash[:last_name][0].downcase == 't'
+    puts "#{hash[:last_name]} #{hash[:first_name]} #{hash[:email]}"
+  end
+end
+  
+ if ary[0][:last_name] == "Theodosius"
+   puts "found the actual value"
+end
+
+#ary.each do |hash|
+#  puts "#{:last_name}"
+#end
+
+#same formatting for the students that have a 't' upper or lower case in their last names
+# loop through each hash
+# find the last name hash[:last_name] key
+# if the last name has a 't' or 'T'
+# use regexp to check if there is an upper or lower case 't'
